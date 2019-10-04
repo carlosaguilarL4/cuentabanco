920726f91d676073367d816715380d49711b4cfd
Carlihno <Carlihno@DESKTOP-906PPP0>
2019-10-04 17:07:39 -0600

    Carpeta Estado de Cuenta Asig8

---
+++ EstadodeCuenta-Asig8/EstadodeCuenta.BL/DatosdeInicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstadodeCuenta.BL
+{
+    public class DatosdeInicio : CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+            {
+                var nuevoUsuario = new Usuario();
+                nuevoUsuario.Nombre = "carlos";
+                nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+
+                contexto.Usuarios.Add(nuevoUsuario);
+
+                base.Seed(contexto);
+            }
+
+
+            {
+                var nuevoUsuario = new Usuario();
+                nuevoUsuario.Nombre = "jorge";
+                nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("456");
+
+                contexto.Usuarios.Add(nuevoUsuario);
+
+                base.Seed(contexto);
+            }
+
+        }
+    }
+
+}
---
+++ EstadodeCuenta-Asig8/EstadodeCuenta.BL/Tipo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstadodeCuenta.BL
+{
+    public class Tipo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese una descripción")]
+        public string Descripcion { get; set; }
+    }
+}
